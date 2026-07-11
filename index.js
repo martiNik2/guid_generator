654b874f4fd2a09fbd3f5e461690d47f8b070227
ZachTheMighty <promoted66@gmail.com>
2026-07-11 23:30:46 +0300

    fix: guid property was accessed incorrectly

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 async function fetchGuid() {
   const response = await fetch("http://127.0.0.1:8000/");
-  const data = response.json();
+  const data = await response.json();
   return data;
 }
+
+const guid = fetchGuid().then((data) => data.guid);
 
 const generateButton = document.querySelector(".generate");
 const guidDiv = document.querySelector(".guid");
 
 generateButton.addEventListener("click", async () => {
-  guidDiv.textContent = await fetchGuid();
+  guidDiv.textContent = await guid;
 });
